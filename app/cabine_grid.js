4977e91218ce3e4b87a16bacbf4f83a39fe9c4ef
timcastelijn <t.castelijn@hotmail.com>
2016-02-16 16:37:15 +0100

    added a door

--- app/cabine_grid.js
+++ app/cabine_grid.js
@@ -18,6 +18,8 @@ function CabineGrid(){
 
   this.walls = [];
 
+  this.position.set(-this.length/2, 0, -this.width/2);
+
   //initialize walls
   var items = this.getWallPositions(this.length, this.width);
 
@@ -25,7 +27,7 @@ function CabineGrid(){
     item = items[i];
 
     //create new wall item
-    var wall = new Wall( item.length, this.height, item.flip);
+    var wall = new Wall( item.length, this.height, item.flip, item.door);
 
     //set wall properties
     wall.position.set(item.position[0], item.position[1], item.position[2]);
@@ -41,17 +43,17 @@ CabineGrid.prototype.constructor = CabineGrid;
 CabineGrid.prototype.getWallPositions=function(length, width){
   var items =[];
 
-  items[0] = {position:[0,0,0], orientation:0, length:length, flip:false};
-  items[1] = {position:[0,0,MODULE_THICKNESS], orientation:-0.5 * Math.PI, length:width, flip:false};
-  items[2] = {position:[length,0,width+2*MODULE_THICKNESS], orientation:-1 * Math.PI, length:length, flip:true};
-  items[3] = {position:[length,0,width+MODULE_THICKNESS], orientation:-1.5 * Math.PI, length:width, flip:true};
+  items[0] = {position:[0,0,0], orientation:0, length:length, flip:false,door:3};
+  items[1] = {position:[-MODULE_THICKNESS,0,width+MODULE_THICKNESS], orientation:0.5 * Math.PI, length:width, flip:false};
+  items[2] = {position:[length,0,width+2*MODULE_THICKNESS], orientation:1 * Math.PI, length:length, flip:false,door:3};
+  items[3] = {position:[length+MODULE_THICKNESS,0,MODULE_THICKNESS], orientation:1.5 * Math.PI, length:width, flip:true};
 
   return items;
 }
 //
 
 //wall class
-function Wall( length, height, flip){
+function Wall( length, height, flip, door){
   THREE.Object3D.call( this );
   //var geometry = new THREE.BoxGeometry(1,1,1);
   //var wall = new THREE.Mesh(geometry);
@@ -59,7 +61,7 @@ function Wall( length, height, flip){
   this.length=length;
   this.height=height;
 
-  var rest_width = this.length%MODULE_WIDTH
+  var rest_width = this.length%MODULE_WIDTH;
 
   var length_offset = 0;
   if(flip){
@@ -68,11 +70,14 @@ function Wall( length, height, flip){
 
   var i;
   for(i=0;i<this.length/MODULE_WIDTH-1; i++){
-    for(var j=0; j<this.height/MODULE_HEIGHT; j++){
-      //define normal cells
-      var cell = new Cell(MODULE_WIDTH, MODULE_HEIGHT, MODULE_THICKNESS);
-      this.add(cell);
-      cell.position.set( (i + 0.5) * MODULE_WIDTH + length_offset, (j +0.5) * MODULE_HEIGHT, 0.5*MODULE_THICKNESS );
+    if(door && i < door){
+    }else{
+      for(var j=0; j<this.height/MODULE_HEIGHT; j++){
+        //define normal cells
+        var cell = new Cell(MODULE_WIDTH, MODULE_HEIGHT, MODULE_THICKNESS);
+        this.add(cell);
+        cell.position.set( (i + 0.5) * MODULE_WIDTH + length_offset, (j +0.5) * MODULE_HEIGHT, 0.5*MODULE_THICKNESS );
+      }
     }
 
     //define rest cells
@@ -96,12 +101,23 @@ Wall.prototype.constructor = Wall;
 
 //CELL Class
 function Cell(width, heigth, thickness){
+  THREE.Object3D.call( this );
 
   //create objects
   //define local variables
-  var geometry = new THREE.BoxGeometry( width, heigth, thickness );
-  var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
-  objects.push( object );
+  // create interior
+  var geometry = new THREE.BoxGeometry( width-0.005, heigth-0.005, thickness/2 );
+  var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0xffffff } ) );
+  object.position.set(0,0,thickness/4);
 
-  return object;
+  // create exterior
+  var geometry = new THREE.BoxGeometry( width, heigth, thickness/2 );
+  var object2 = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0x111111 } ) );
+  object2.position.set(0,0,-thickness/4);
+
+  this.add( object );
+  this.add( object2 );
+
 }
+Cell.prototype = new THREE.Object3D();
+Cell.prototype.constructor = Cell;
